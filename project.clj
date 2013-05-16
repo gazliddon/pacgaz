3dba57611cd855a77306f75d1296d65333aaa11a
Gaz Liddon Retina <gaz@gazaxian.com>
2013-05-16 08:10:22 +0100

    Rewritten
    Now loads in XLS files, works completely differently but public API is
    still the same

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [xls2json "1.0.1"]
+                 [dk.ative/docjure "1.6.0"]
                  [penumbra "0.6.0"]])
 
 
--- src/pacgaz/mapdata.clj
+++ src/pacgaz/mapdata.clj
@@ -1,8 +1,9 @@
-(ns pacgaz.mapdata
-  (:require clojure.pprint)
-  (:require [dk.ative.docjure.spreadsheet :as dj])
-  (:use [clojure.string :only (join)]))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(ns pacgaz.mapdata
+  (:use [clojure.string :only (join)])
+  (:require [dk.ative.docjure.spreadsheet :as dj] ))
+ 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Different map element types
 
@@ -12,54 +13,84 @@
     \- { :name "space"  :collide false }
     \. { :name "pill"   :collide false }
     \P { :name "pacman" :collide false }
+    \! { :name "error"  :collide false }   ;; Unknown piece
     })
 
 ;; Valid bit for unrecognised chars in the map data
-(def err-bit { :name "error" :collide false})
-(defn- get-map-bit-type [c] (get map-bit-types c err-bit))
+(defn- get-map-bit-type [c] (get map-bit-types c (get map-bit-types \!)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Turn a string into an array of map bits
-;; Parse the map from a string, like the one at the bottom of this file
-;; and turn it into a sequence
-(defn- char-to-map-data [w idx chr]
-  (merge { :x (rem idx w) :y (quot idx w) } (get-map-bit-type chr)))
- 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Public api here
+;; Excel file magubbins
 
+(defn- get-cell-data
+  "Dump an excel cell into x,y and text"
+  [cell]
+  (let [x    (.getRowIndex cell)
+        y    (.getColumnIndex cell)
+        text (.getStringCellValue cell)]
 
-(defn load-level
-  "Loads a level from an XLSX file
-   assumes that the map is in the first sheet"
-  [file]
-
-  (defn row-to-cells [w row]
-    (let [ cell-data (vec (map dj/read-cell (dj/cell-seq row))) ]
-      (map  #(get cell-data % " ") (range w))))
-  
-  (let [wb   (dj/load-workbook file )
-        rows (dj/row-seq  (first (dj/sheet-seq wb)))
-        w    (apply max (map  #(count (dj/cell-seq %)) rows))
-        h    (count rows)
-        joined (join (flatten (map (partial row-to-cells w) rows)))
-        map-bits (map-indexed (partial char-to-map-data w) joined)
-        ]
-    {:height h  :width w
-     :joined joined
-     :map map-bits
-     })
+    {:x x :y y :text text})
   )
 
+(defn- iterate-cells
+  "Iterate through all of the cells in the first sheet of this workbook"
+  [file func]
+  (defn dump-row [row]
+    (map func (dj/cell-seq row)))
+  
+  (let [rows (-> file
+                 (dj/load-workbook)
+                 (first)
+                 (dj/row-seq))]
+    (flatten (map dump-row rows))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Transform into game data functions
+
+(defn- get-map-bit-cell [cell]
+  (let [ret-cell (get-cell-data cell)
+        chr (first (:text ret-cell))
+        map-bit (get-map-bit-type chr)]
+    (merge ret-cell map-bit)))
+ 
+(defn- xls-to-tiles
+  "Turn an xls file into a bunch of map bit tiles {:x :y :map-bit}"
+  [file]
+ (iterate-cells file get-map-bit-cell))
+ 
+(defn- tiles-to-hash
+  "Turn a sequence of tiles into a hash keyed by [x y]"
+  [tiles]
+  (reduce #(assoc %1 [ (:x %2) (:y %2) ] %2) {} tiles))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Public API
+
+;; I really like the reduce to make a hash map keyed by tile [x y] pos
+(defn load-level
+  "Turn an xls file into a level"
+  [file]
+  (let [ tiles       (iterate-cells file get-map-bit-cell)
+         pos-to-bit  (reduce #(assoc %1 [ (:x %2) (:y %2) ] %2) {} tiles)
+        ]
+    { :tiles      tiles
+      :pos-to-bit pos-to-bit }))
+
 (defn get-tiles-of-type
-  "get all of the tiles of this named type from the map"
+  "Get all of the tiles of this named type from the map"
   [level type]
-  (filter #(= type (:name %)) (level :map)))
+  (filter #(= type (:name %)) (:tiles level)))
 
 (defn get-tile
-  "get the tile at x,y from this map"
+  "Get the tile at x,y from this map"
   [level x y]
   (assert false)
   )
 
 ;;; ends
+
+;;; Todo?
+;;; Turn add hoc maps into Records?
+;;; Make level into a protocol
